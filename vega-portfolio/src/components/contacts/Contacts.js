c0f77734cb07a7c9bff7eaa228b2eb4719cb7687
VegaCristian-FS <cavega@student.fullsail.edu>
2022-06-25 15:36:02 -0400

    predeploying code

--- vega-portfolio/src/components/contacts/Contacts.js
+++ vega-portfolio/src/components/contacts/Contacts.js
@@ -1,49 +1,53 @@
-import React from 'react'
+import React, {useRef, useState} from 'react'
+import './contacts.css'
+import {AiOutlineMail, AiOutlinePhone} from 'react-icons/ai'
+import {GoLocation} from 'react-icons/go'
+import emailjs from 'emailjs-com';
+
 
 function Contacts() {
+  const formRef = useRef()
+  const [done, setDone] = useState(false)
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+    
+    emailjs.sendForm(
+      'service_plukz9a', 
+      'template_jis4l7h', 
+      formRef.current, 
+      '8siY23Uyt2-cA2tkw'
+      )
+      .then((result) => {
+          console.log(result.text);
+          setDone(true);
+      }, (error) => {
+          console.log(error.text);
+      });
+  }
+  
   return (
-    <div className='contacts'>
+    <div id='contacts'>
         <article className="contacts-left">
           <h2>Contact Information</h2>
           <h3>Have Any Questions?</h3>
-          <p>Best way to reach me and its many options. I will answer any questions you have.</p>
+          <p className='contacts-info-desc'>Best way to reach me and its many options. I will answer any questions you have.</p>
+          <div className='contact-info'>
+            <p><AiOutlinePhone /> 813-459-1471</p> 
+            <p><AiOutlineMail /> Cr_Vega23@outlook.com</p> 
+            <p><GoLocation /> Tampa, Fl (Remote)</p> 
+          </div>
         </article>
          
         <article className="contacts-right">
-          <h2>Contact Us</h2>
-          <h3>Send Us An Email</h3>
-          <form action="/my-form">
-            <div class="form-group">
-              <div class="">
-                <label for="fname" class="col-form-label">First Name</label>
-                <input type="text" class="form-control" id="fname" placeholder="John"/>
-              </div>
-              <div class="col-6">
-                <label for="lname" class="col-form-label">Last Name</label>
-                <input type="text" class="form-control" id="lname" placeholder="Doe"/>
-              </div>
-            </div>
-            <div class="form-group row">
-              <div class="col-6">
-                <label for="email" class="col-form-label">Email</label>
-                <input type="email" class="form-control" id="email" placeholder="Email"/>
-              </div>
-              <div class="col-6">
-                <label for="subject" class="col-form-label">Subject</label>
-                <input type="text" class="form-control" id="subject" placeholder="About..."/>
-              </div>
-            </div>
-            <div class="form-group row">
-              <div class="col">                
-                <label class="col-form-label" for="message">Message</label>
-                <textarea class="form-control" name="message" id="message"></textarea>
-              </div>
-            </div>
-            <div class="form-group row ">
-              <div class="text-center">
-                <button type="submit" class="btn btn-primary m-3">Send Message</button>
-              </div>
-            </div>
+          <h2>Contact Me</h2>
+          <h3>Send Me An Email</h3>
+          <form ref={formRef} onSubmit={handleSubmit}>
+            <input type='text' placeholder='Name' name='user_name' />
+            <input type='text' placeholder='Subject' name='user_subject' />
+            <input type='text' placeholder='Email' name='user_email' />
+            <textarea rows='5' placeholder='Message' name='message'/>
+            <button>Submit</button>
+            {done && 'Email was sent!'}
           </form>
         </article>
     </div>
